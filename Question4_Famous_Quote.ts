b68e3148787a4bc861c46078a0589e9de643d813
Muhib Ahmed <160688677+Muhib656@users.noreply.github.com>
2024-03-30 11:11:51 +0500

    Print a famous quote

---
+++ Question4_Famous_Quote.ts
@@ -0,0 +1,7 @@
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author.
+// Your output should look something like the following, including the quotation marks:
+
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+
+console.log("Willaim Shakespeare once said,\"If we are true to ourselves, we cannot be false to anyone\"")
