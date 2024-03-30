99d9617886c7292ddc526564a7f91f734dba1871
Muhib Ahmed <160688677+Muhib656@users.noreply.github.com>
2024-03-30 11:29:49 +0500

    My first commit

---
+++ Question23_Boolean-test.ts
@@ -0,0 +1,61 @@
+// Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+
+// let car = 'subaru';
+
+// console.log("Is car == 'subaru'? I predict True.")
+
+// console.log(car == 'subaru')
+
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False
+
+
+// Making Variables
+let five: number = 5;
+
+let ten: number = 10;
+
+// test 1
+console.log("test 1: five is equal to 5?" );
+console.log(five == 5);
+
+//test 2
+console.log("\ntest 2: ten is equal to 10?");
+console.log(ten == 10);
+
+//test 3
+console.log("\ntest 3: five is not equal to 10?");
+console.log(5 != ten);
+
+// test 4
+console.log("\ntest 4: 10 is greater than 5");
+console.log(ten > five);
+
+//test 5"
+console.log("\ntest 5: five is smaller than 10?");
+console.log(5 < 10);
+
+//test 6
+console.log("\ntest 6: five is equal to 10?");
+console.log(5 == ten);
+
+//test 7
+console.log("\ntest 7: 5 is greater than 10?");
+console.log(five > 10)
+
+//test8
+console.log("\ntest 8: ten is smaller than 5?");
+console.log(10 < 5);
+
+//test 9
+console.log("\n10 is equal to 5?");
+console.log(ten == five)
+
+// test 9
+console.log("\ntest 10: 10 is not equal to ten");
+console.log(ten != 10);
+
+// test 11 
+console.log("\ntest 11: 50 is greater than 100?");
+console.log(50 > 100);
