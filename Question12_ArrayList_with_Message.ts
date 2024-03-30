75f6f30585b9845116808d5e9f5c01b9fbbb1d78
Muhib Ahmed <160688677+Muhib656@users.noreply.github.com>
2024-03-30 11:21:48 +0500

    My first commit

---
+++ Question12_ArrayList_with_Message.ts
@@ -0,0 +1,12 @@
+// Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, 
+// print a message to them. 
+// The text of each message should be the same, but each message should be personalized with the person’s name.
+
+// Using BackTicks " ` "
+
+
+
+
+let freindsName = ["Anza", "Amania", "Bilal", "Faisal"];
+
+freindsName.forEach(freindName => console.log(`Hello ${freindName}, Hope you are fine`));
