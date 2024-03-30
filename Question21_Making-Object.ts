990da0fdc96888b6eee881d55ffc67768aaa79da
Muhib Ahmed <160688677+Muhib656@users.noreply.github.com>
2024-03-30 11:28:18 +0500

    My first commit

---
+++ Question21_Making-Object.ts
@@ -0,0 +1,23 @@
+// They think of something you could store in a TypeScript Object. 
+// Write a program that creates Objects containing these item
+
+
+// Making interface of the object
+
+interface itCourse {
+    courseName: string;
+    location : string;
+    onSiteStudent : number;
+};
+
+
+    // Making object
+
+    let itCourse = {
+    courseName : "Cloud computing and Gen AI",
+    location : "Governor House Sindh",
+    onSiteStudent : 50000
+
+    };
+
+    console.log(itCourse);
