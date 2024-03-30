39d46139c0515fadc2767807fc18558489dd82ed
Muhib Ahmed <160688677+Muhib656@users.noreply.github.com>
2024-03-30 11:17:50 +0500

    My first commit

---
+++ Question10_Adding_Comments.ts
@@ -0,0 +1,19 @@
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. 
+// If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. 
+// Then write one sentence describing what the program does
+
+
+
+
+
+console.log(5 + 3);
+// Addition
+
+console.log(15 - 7);
+// Substraction
+
+console.log(4 * 2);
+// Multiplication
+
+console.log(24 / 3);
+// Division
