473336a6494f3048423d06466b9238318a9cc3e4
Muhib Ahmed <160688677+Muhib656@users.noreply.github.com>
2024-03-30 11:13:20 +0500

    Print a message of a famous person

---
+++ Question5_FamousPerson_Quote.ts
@@ -0,0 +1,10 @@
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. 
+// Then compose your message and store it in a new variable called message. Print your message
+
+
+
+
+
+let famousPerson: string = "William Shakespeare";
+let quote: string = "once said, 'If we are true to ourselves, we cannot be false to anyone'";
+console.log(famousPerson, quote)
