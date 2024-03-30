2db00fc174653d2c1cfeef69a434212770fc1ac5
Muhib Ahmed <160688677+Muhib656@users.noreply.github.com>
2024-03-30 11:27:34 +0500

    My first commit

---
+++ Question20_List-of-Mountains.ts
@@ -0,0 +1,15 @@
+// Think of something you could store in a array. 
+// For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. 
+// Write a program that creates a list containing these items
+
+
+
+
+// Making the favourite mountains array
+let favMountains: string[] = ["Nanga Parbat", "Rakaposhi", "Mount Everest", "Laila Peak"];
+
+// Print the message of list
+console.log("List of favMountains:");
+
+// Print the values of array in form of lst, Using forEach method
+favMountains.forEach(mountains => console.log(mountains));
