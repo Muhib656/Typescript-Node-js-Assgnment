205eee936816b1bba2ae16040ee82b84bb12cc79
Muhib Ahmed <160688677+Muhib656@users.noreply.github.com>
2024-03-30 11:18:35 +0500

    My first commit

---
+++ Question11_Array_List.ts
@@ -0,0 +1,9 @@
+// Names: Store the names of a few of your friends in a array called names. 
+// Print each person’s name by accessing each element in the list, one at a time.
+
+//Using forEach method
+
+
+let freindsName = ["Anza", "Amania", "Bilal", "Faisal"];
+
+freindsName.forEach(freindName => console.log(freindName));
