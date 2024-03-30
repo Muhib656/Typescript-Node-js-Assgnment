355271d90e60595cdb2c259ff09000b4f36154a4
Muhib Ahmed <160688677+Muhib656@users.noreply.github.com>
2024-03-30 11:45:42 +0500

    Question43-My first commit

--- great_magician.ts
+++ great_magician.ts
@@ -1,13 +1,19 @@
+/*Unchanged Magicians: Start with your work from Exercise 40. Call the function make_great() with a copy of the array of magicians’ names.
+ Because the original array will be unchanged, return the new array and store it in a separate array.
+ Call show_magicians() with each array to show that you have one array of the original names and one array with the Great added to each magician’s name.*/
 
-// Great Magicians: Start with a copy of your program from Exercise 39. 
-// Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name. 
-// Call show_magicians() to see that the list has actually been modified.
 
 let magician: string[] = ["Harry", "Chris", "Leo", "Paul"];
 
+function copyArray( copyMagicianArray: string[]){
+
+    return [...copyMagicianArray]
+}
 
 function make_great (magicianArray: string[]){
+
     for(let i=0; i<magicianArray.length; i++ ){
+
         magicianArray[i] = 'The Great '  +  magicianArray[i]
     }
 }
@@ -18,5 +24,12 @@ function show_magicians(magicians: string []){
       });
 }
 
-make_great(magician)
+const copyMagicianArray = copyArray(magician)
+
+make_great(copyMagicianArray)
+
+console.log('\n\nThis is my Original Array')
 show_magicians(magician);
+
+console.log('\n\nThis is modfied copy of Array')
+show_magicians(copyMagicianArray);
