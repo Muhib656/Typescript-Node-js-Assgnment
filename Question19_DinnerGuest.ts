68c303a0410a9f5219417448af63fc38d02738ae
Muhib Ahmed <160688677+Muhib656@users.noreply.github.com>
2024-03-30 11:26:57 +0500

    My first commit

---
+++ Question19_DinnerGuest.ts
@@ -0,0 +1,14 @@
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, 
+// print a message indicating the number of people you are inviting to dinner
+
+
+
+
+let guestList = ["Saad", "Bilal", "Monee", "Noman"];
+
+// guestList.forEach(oneGuest => console.log(`Salam ${oneGuest}, would you like to dinner with me?`))
+
+// we can find the total length of the array
+let lengthGuest: Number = guestList.length;
+
+console.log(`We are inviting total ${lengthGuest} guests`);
