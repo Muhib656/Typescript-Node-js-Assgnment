28e2c738e4c22a0a168f0714f3b38eaae54ceff5
Muhib Ahmed <160688677+Muhib656@users.noreply.github.com>
2024-03-30 11:22:28 +0500

    My first commit

---
+++ Question13_CallingSeries_Array.ts
@@ -0,0 +1,9 @@
+// Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. 
+// Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+
+// Calling diffrent values of array in same statement
+
+
+let transposrtationModes = ["Bike", "Car","Yatch","Helicopter"];
+
+transposrtationModes.forEach(mode => console.log (`I would like to own a ${mode}`));
