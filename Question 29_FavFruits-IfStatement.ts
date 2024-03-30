c59c182e29418a07b31c5123bb28eb3df53b94b4
Muhib Ahmed <160688677+Muhib656@users.noreply.github.com>
2024-03-30 11:33:46 +0500

    My first commit

---
+++ Question 29_FavFruits-IfStatement.ts
@@ -0,0 +1,29 @@
+// Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your array.
+// • Make a array of your three favorite fruits and call it favorite_fruits.
+
+// • Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement, such as You really like bananas!
+
+
+// create an array
+let favouriteFruit = ["Mango", "Banana", "Orange"]
+
+// creating 5 if statements
+if (favouriteFruit.includes("Mango")){
+    console.log("I like Mangoes")
+};
+
+if (favouriteFruit.includes("Banana")){
+    console.log("I like Banana")
+};
+
+if (favouriteFruit.includes('Apple')){
+    console.log("I like Apples")
+};
+
+if (favouriteFruit.includes("Orange")){
+    console.log("I like Oranges")
+};
+
+if (favouriteFruit.includes("Strawberry")){
+    console.log("I like Strawbwerries")
+};
