f2ab7ae303f2d8e1143b6142e187fcedcbac2305
Muhib Ahmed <160688677+Muhib656@users.noreply.github.com>
2024-03-30 11:28:58 +0500

    My first commit

---
+++ Question22_Error-Index.ts
@@ -0,0 +1,14 @@
+// Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. 
+// Change an index in one of your programs to produce an index error. 
+// Make sure you correct the error before closing the program.
+
+
+
+let erreoArray: string[] = ["A", "B", "C", "D"];
+
+// Producing error ! by accessing invaid index of array
+
+// console.log(erreoArray[5]);
+
+//Error Removed
+console.log(erreoArray[2]);
